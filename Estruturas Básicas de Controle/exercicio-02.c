6774208bc738ea225cad4c15bf2eae3cd3dcac18
Mehl <mehlsampaio@gmail.com>
2025-08-29 10:50:54 -0300

    Adiciona arquivo exercicio-02.c

---
+++ Estruturas Básicas de Controle/exercicio-02.c
@@ -0,0 +1,23 @@
+// Escreva um programa que receba o número total de gols de um jogador
+// e a quantidade de partidas jogadas. Depois, calcule e exiba
+// a média de gols por partida. 
+
+#include <stdio.h>
+
+int main() {
+	int totalGols;
+	int qtdPartidas; 
+	float mediaGols;
+	
+	printf("Digite o total de gols feitos: ");
+	scanf("%d", &totalGols);
+	
+	printf("Digite a quantidade de partidas jogadas: ");
+	scanf("%d", &qtdPartidas); 
+	
+	mediaGols = totalGols / qtdPartidas;
+	
+	printf("A media de gols por partida eh: %.2f", mediaGols);
+	
+	return 0;
+}
